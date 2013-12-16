7fa530780b7ec8b25b7dd89abdbf9f23f9243ea1
Jiahao Li <isundaylee.reg@gmail.com>
2013-12-16 03:32:09 -0500

    Initial working.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'simple_cache', path: '/Users/Sunday/Programming/Git/simple_cache'
+
+gem 'feedzirra'
---
+++ download.rb
@@ -0,0 +1,41 @@
+require 'bundler/setup'
+
+require 'simple_cache'
+require 'feedzirra'
+require 'fileutils'
+
+OUTPUT_DIR = 'output'
+CACHE_DIR = File.expand_path('~/Library/Caches/railscasts-download-cache')
+FEED_URL = 'http://railscasts.com/subscriptions/6MT8ikJqpLaQxbZ9ICXKyw/episodes.rss'
+
+cacher = SimpleCache::Cacher.new(CACHE_DIR)
+
+puts 'Fetching the feed list ...'
+# I do not know why I insist on using a cacher on this...
+# If you know, do tell me! 
+feed_content = cacher.retrieve_by_url(FEED_URL, expiration: 100)
+
+feed = Feedzirra::Feed.parse(feed_content)
+
+feed.entries.reverse.each do |entry|
+  regex = /^#([0-9]*)\s(.*)$/
+  match = regex.match(entry.title)
+
+  raise 'Illformed title. ' unless match
+
+  num = match[1].to_i
+  title = match[2]
+
+  path = File.join(OUTPUT_DIR, "%03d %s.mp4" % [num, title])
+
+  if File.exists?(path)
+    puts "Skipping #{entry.title}"
+  else
+    url = entry.enclosure_url
+    puts "Downloading #{entry.title} ..."
+    content = cacher.retrieve_by_url(url, show_progress: true)
+    FileUtils.mkdir_p(OUTPUT_DIR)
+    File.write(path + '.tmp', content)
+    FileUtils.mv(path + '.tmp', path)
+  end
+end
